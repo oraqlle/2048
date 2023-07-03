adefc2fb3f1aa8e826b9e46e3974a4b7b5df2e84
oraqlle <41113853+oraqlle@users.noreply.github.com>
2023-07-03 13:50:38 +1000

    Rewrote Rust version to use a more functional style (mirrors Elixir).

--- 2048-rust/src/main.rs
+++ 2048-rust/src/main.rs
@@ -1,5 +1,7 @@
-use rand::random;
-use std::{io, ops::AddAssign, thread, time::Duration};
+use rand::{seq::SliceRandom, Rng};
+use std::{collections::HashMap, io, thread, time::Duration};
+
+type Board = HashMap<(u8, u8), usize>;
 
 #[derive(Debug)]
 enum Move {
@@ -10,94 +12,74 @@ enum Move {
     Quit,
 }
 
-fn index(x: usize, y: usize) -> usize {
-    (y * 4) + x
-}
-
-fn print_board(field: &mut Vec<usize>) -> Result<(), &'static str> {
-    let width = field
+fn print_board(board: &Board) {
+    let width = board
         .iter()
+        .by_ref()
+        .map(|(_, v)| v)
         .max()
         .and_then(|x| Some(x.to_string()))
-        .and_then(|x| Some(x.len() + 2));
+        .and_then(|x| Some(x.len() + 2))
+        .unwrap_or(3);
 
-    let spacing = String::from("─").repeat(width.unwrap_or(3));
+    let spacing = String::from("─").repeat(width);
 
     println!("┌{0}┬{0}┬{0}┬{0}┐", spacing);
 
-    for ridx in 0..3 {
-        print!("│");
+    (0..3).for_each(|y| {
+        let row = (0..4).fold(String::from(""), |mut row, x| {
+            let tile = board.get(&(x, y)).unwrap().clone();
+            let size = tile.to_string().len();
 
-        for cidx in 0..4 {
-            let idx = index(cidx, ridx);
+            let ws_size = width - size - 2usize;
+            let right_ws_size = ws_size / 2usize;
+            let left_ws_size = ws_size - right_ws_size;
 
-            match field.get(idx) {
-                Some(cell) => {
-                    let size = cell.to_string().len();
-                    let ws_size = width.unwrap_or(3) - size - 2;
-                    let right_ws_size = ws_size / 2;
-                    let left_ws_size = ws_size - right_ws_size;
+            let right_ws = String::from(" ").repeat(right_ws_size);
+            let left_ws = String::from(" ").repeat(left_ws_size);
 
-                    let right_ws = String::from(" ").repeat(right_ws_size);
-                    let left_ws = String::from(" ").repeat(left_ws_size);
-
-                    if cell == &0 {
-                        print!(" {} {} │", left_ws, right_ws);
-                    } else {
-                        print!(" {}{}{} │", left_ws, cell, right_ws);
-                    }
-                }
-                None => {
-                    let err: &'static str = "Index out of range.";
-                    return Err(err);
-                }
+            if tile == 0 {
+                row.extend(format!(" {left_ws} {right_ws} │").chars());
+            } else {
+                row.extend(format!(" {left_ws}{tile}{right_ws} │").chars());
             }
+
+            return row;
+        });
+
+        println!("│{}", row);
+        println!("├{spacing}┼{spacing}┼{spacing}┼{spacing}┤");
+    });
+
+    let row = (0..4).fold(String::from(""), |mut row, x| {
+        let tile = board.get(&(x, 3)).unwrap().clone();
+        let size = tile.to_string().len();
+
+        let ws_size = width - size - 2usize;
+        let right_ws_size = ws_size / 2usize;
+        let left_ws_size = ws_size - right_ws_size;
+
+        let right_ws = String::from(" ").repeat(right_ws_size);
+        let left_ws = String::from(" ").repeat(left_ws_size);
+
+        if tile == 0 {
+            row.extend(format!(" {left_ws} {right_ws} │").chars());
+        } else {
+            row.extend(format!(" {left_ws}{tile}{right_ws} │").chars());
         }
-        println!("\n├{0}┼{0}┼{0}┼{0}┤", spacing);
-    }
 
-    print!("│");
+        return row;
+    });
 
-    for cidx in 0..4 {
-        let idx = index(cidx, 3);
-
-        match field.get(idx) {
-            Some(cell) => {
-                let size = cell.to_string().len();
-                let ws_size = width.unwrap_or(3) - size - 2;
-                let right_ws_size = ws_size / 2;
-                let left_ws_size = ws_size - right_ws_size;
-
-                let right_ws = String::from(" ").repeat(right_ws_size);
-                let left_ws = String::from(" ").repeat(left_ws_size);
-
-                if cell == &0 {
-                    print!(" {} {} │", left_ws, right_ws);
-                } else {
-                    print!(" {}{}{} │", left_ws, cell, right_ws);
-                }
-            }
-            None => {
-                let err: &'static str = "Index out of range.";
-                return Err(err);
-            }
-        }
-    }
-    println!("\n└{0}┴{0}┴{0}┴{0}┘", spacing);
-
-    Ok(())
+    println!("│{}", row);
+    println!("└{spacing}┴{spacing}┴{spacing}┴{spacing}┘");
 }
 
-fn print_game(
-    field: &mut Vec<usize>,
-    score: &usize,
-    verbose: bool,
-    lost: bool,
-) -> Result<(), &'static str> {
+fn show(board: &Board, score: usize, verbose: bool) -> Result<(), String> {
     match clearscreen::clear() {
         Ok(()) => {}
-        Err(_) => return Err("Failed to clear screen."),
-    }
+        Err(_) => return Err(String::from("Failed to clear screen.")),
+    };
 
     println!("2048");
 
@@ -115,258 +97,201 @@ fn print_game(
         println!("Score: {}\n", score);
     }
 
-    print_board(field)?;
-
-    if lost {
-        println!("\nGame Over!");
-        println!("Final Score: {}", score)
-    }
-
+    print_board(board);
     Ok(())
 }
 
-fn do_game_step(
-    umove: &Move,
-    field: &mut Vec<usize>,
-    score: &mut usize,
-) -> Result<(), &'static str> {
-    match umove {
-        &Move::Up => {
-            for cidx in 0..4 {
-                for ridx in 0..4 {
-                    for next in (ridx + 1)..4 {
-                        if field[index(cidx, next)] != 0 {
-                            if field[index(cidx, ridx)] == 0 {
-                                field[index(cidx, ridx)] += field[index(cidx, next)];
-                                field[index(cidx, next)] = 0;
-                            } else if field[index(cidx, ridx)] == field[index(cidx, next)] {
-                                field[index(cidx, ridx)] += field[index(cidx, next)];
-                                field[index(cidx, next)] = 0;
-
-                                score.add_assign(field[index(cidx, ridx)]);
-                                break;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        &Move::Left => {
-            for ridx in 0..4 {
-                for cidx in 0..4 {
-                    for next in (cidx + 1)..4 {
-                        if field[index(next, ridx)] != 0 {
-                            if field[index(cidx, ridx)] == 0 {
-                                field[index(cidx, ridx)] += field[index(next, ridx)];
-                                field[index(next, ridx)] = 0;
-                            } else if field[index(cidx, ridx)] == field[index(next, ridx)] {
-                                field[index(cidx, ridx)] += field[index(next, ridx)];
-                                field[index(next, ridx)] = 0;
-
-                                score.add_assign(field[index(cidx, ridx)]);
-                                break;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        &Move::Down => {
-            for cidx in 0..4 {
-                for ridx in (0..4).rev() {
-                    for next in (0..ridx).rev() {
-                        if field[index(cidx, next)] != 0 {
-                            if field[index(cidx, ridx)] == 0 {
-                                field[index(cidx, ridx)] += field[index(cidx, next)];
-                                field[index(cidx, next)] = 0;
-                            } else if field[index(cidx, ridx)] == field[index(cidx, next)] {
-                                field[index(cidx, ridx)] += field[index(cidx, next)];
-                                field[index(cidx, next)] = 0;
-
-                                score.add_assign(field[index(cidx, ridx)]);
-                                break;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        &Move::Right => {
-            for ridx in 0..4 {
-                for cidx in (0..4).rev() {
-                    for next in (0..cidx).rev() {
-                        if field[index(next, ridx)] != 0 {
-                            if field[index(cidx, ridx)] == 0 {
-                                field[index(cidx, ridx)] += field[index(next, ridx)];
-                                field[index(next, ridx)] = 0;
-                            } else if field[index(cidx, ridx)] == field[index(next, ridx)] {
-                                field[index(cidx, ridx)] += field[index(next, ridx)];
-                                field[index(next, ridx)] = 0;
-
-                                score.add_assign(field[index(cidx, ridx)]);
-                                break;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        _ => return Err("Invalid input."),
-    }
-
-    Ok(())
-}
-
-fn get_user_move() -> Result<Move, &'static str> {
-    let umove;
-
-    'inputloop: loop {
+fn input() -> Result<Move, String> {
+    loop {
         let mut uinput = String::new();
-
         match io::stdin().read_line(&mut uinput) {
-            Err(_) => {
-                let msg: &'static str = "Failed to get user input.";
-                return Err(msg);
-            }
-            Ok(_) => match uinput.chars().nth(0) {
-                Some('w') => {
-                    umove = Move::Up;
-                    break 'inputloop;
-                }
-                Some('a') => {
-                    umove = Move::Left;
-                    break 'inputloop;
-                }
-                Some('s') => {
-                    umove = Move::Down;
-                    break 'inputloop;
-                }
-                Some('d') => {
-                    umove = Move::Right;
-                    break 'inputloop;
-                }
-                Some('q') => {
-                    umove = Move::Quit;
-                    break 'inputloop;
-                }
-                Some(other) => {
-                    println!("Invalid input: {}. Valid inputs are w-a-s-d and q.", other);
-                    thread::sleep(Duration::from_secs(1));
-                }
-                _ => {
-                    panic!("Unrecognized error!");
-                }
-            },
-        }
-    }
+            Err(_) => return Err(String::from("Failed to get user input.")),
+            Ok(_) => {
+                return {
+                    match uinput.chars().nth(0) {
+                        Some('w') => Ok(Move::Up),
+                        Some('a') => Ok(Move::Left),
+                        Some('s') => Ok(Move::Down),
+                        Some('d') => Ok(Move::Right),
+                        Some('q') => Ok(Move::Quit),
+                        Some(other) => Err(format!(
+                            "Invalid input: {}. Valid inputs are w-a-s-d and q.",
+                            other
+                        )),
 
-    Ok(umove)
+                        _ => Err(String::from("Unrecognized error!")),
+                    }
+                }
+            }
+        };
+    }
 }
 
-fn gen_rand_cell(field: &mut Vec<usize>) -> Result<(), &'static str> {
-    'randloop: loop {
-        let rnd = random::<usize>();
-        let idx = index((rnd / 4) % 4, rnd % 4);
+fn play_move(board: &Board, score: usize, mv: Move) -> (Board, usize) {
+    match mv {
+        Move::Up => (0..4).fold((board.clone(), score), |(brd, s), x| {
+            let tiles = (0..4).map(|y| brd[&(x, y)]).collect::<Vec<_>>();
+            let (new_tiles, new_score) = move_and_combine(tiles.as_slice(), s);
 
-        match field.get_mut(idx) {
-            None => {
-                return Err("Index out of range.");
+            (
+                new_tiles
+                    .iter()
+                    .enumerate()
+                    .fold(brd, |mut new_board, (y, &e)| {
+                        new_board.insert((x, y as u8), e);
+                        return new_board;
+                    }),
+                new_score,
+            )
+        }),
+        Move::Left => (0..4).fold((board.clone(), score), |(brd, s), y| {
+            let tiles = (0..4).map(|x| brd[&(x, y)]).collect::<Vec<_>>();
+            let (new_tiles, new_score) = move_and_combine(tiles.as_slice(), s);
+
+            (
+                new_tiles
+                    .iter()
+                    .enumerate()
+                    .fold(brd, |mut new_board, (x, &e)| {
+                        new_board.insert((x as u8, y), e);
+                        return new_board;
+                    }),
+                new_score,
+            )
+        }),
+        Move::Down => (0..4).fold((board.clone(), score), |(brd, s), x| {
+            let tiles = (0..4).rev().map(|y| brd[&(x, y)]).collect::<Vec<_>>();
+            let (new_tiles, new_score) = move_and_combine(tiles.as_slice(), s);
+
+            (
+                new_tiles
+                    .iter()
+                    .rev()
+                    .enumerate()
+                    .fold(brd, |mut new_board, (y, &e)| {
+                        new_board.insert((x, y as u8), e);
+                        return new_board;
+                    }),
+                new_score,
+            )
+        }),
+        Move::Right => (0..4).fold((board.clone(), score), |(brd, s), y| {
+            let tiles = (0..4).rev().map(|x| brd[&(x, y)]).collect::<Vec<_>>();
+            let (new_tiles, new_score) = move_and_combine(tiles.as_slice(), s);
+
+            (
+                new_tiles
+                    .iter()
+                    .rev()
+                    .enumerate()
+                    .fold(brd, |mut new_board, (x, &e)| {
+                        new_board.insert((x as u8, y), e);
+                        return new_board;
+                    }),
+                new_score,
+            )
+        }),
+        _ => panic!(""),
+    }
+}
+
+fn move_and_combine(tiles: &[usize], score: usize) -> ([usize; 4], usize) {
+    let new_tiles = tiles
+        .iter()
+        .cloned()
+        .filter(|&e| e > 0usize)
+        .chain([0usize; 4])
+        .take(4)
+        .collect::<Vec<_>>();
+
+    match *new_tiles.as_slice() {
+        [a, b, c, d] if (a == b) && (c == d) => (
+            [a * 2usize, c * 2usize, 0usize, 0usize],
+            score + (a * 2usize) + (c * 2usize),
+        ),
+        [a, b, c, d] if (a == b) => ([a * 2usize, c, d, 0usize], score + (a * 2usize)),
+        [a, b, c, d] if (b == c) => ([a, b * 2usize, d, 0usize], score + (b * 2usize)),
+        [a, b, c, d] if (c == d) => ([a, b, c * 2usize, 0usize], score + (c * 2usize)),
+        [a, b, c, d] => ([a, b, c, d], score),
+        [] | [_, ..] => ([0usize; 4], score),
+    }
+}
+
+fn combinable(board: &Board) -> bool {
+    (0..4)
+        .map(|x| (0..3).map(move |y| board[&(x, y)] == board[&(x, y + 1u8)]))
+        .flatten()
+        .any(|b| b)
+        || (0..4)
+            .map(|x| (0..3).map(move |y| board[&(x, y)] == board[&(x + 1u8, y)]))
+            .flatten()
+            .any(|b| b)
+}
+
+fn setup() -> Board {
+    let mut board = (0..4)
+        .map(|x| (0..4).map(move |y| ((x, y), 0usize)))
+        .flatten()
+        .collect::<HashMap<(u8, u8), usize>>();
+
+    add_random_tile(&mut board);
+    add_random_tile(&mut board);
+
+    return board;
+}
+
+fn add_random_tile(board: &mut Board) {
+    let mut rng = rand::thread_rng();
+    let pos = *blank_spaces(board).choose(&mut rng).unwrap();
+    let tile = if rng.gen_range(0..10) == 1 { 4 } else { 2 };
+    board.insert(pos, tile);
+}
+
+fn blank_spaces(board: &Board) -> Vec<(u8, u8)> {
+    board
+        .iter()
+        .filter_map(|(&k, &v)| if v == 0usize { Some(k) } else { None })
+        .collect::<Vec<_>>()
+}
+
+fn play(board: Board, score: usize) {
+    match show(&board, score, false) {
+        Ok(()) => {}
+        Err(e) => panic!("{e}"),
+    };
+
+    if board.values().any(|&v| v == 0) || combinable(&board) {
+        match input() {
+            Ok(Move::Quit) => {
+                println!("Game Over!");
+                println!("Final Score: {score}");
+                return ();
             }
-            Some(cell) => {
-                if *cell == 0 {
-                    if rnd % 10 == 0 {
-                        *cell = 4;
-                    } else {
-                        *cell = 2;
-                    }
-                    break 'randloop;
+            Ok(mv) => {
+                let (mut moved, new_score) = play_move(&board, score, mv);
+
+                if moved == board {
+                    play(board, new_score);
+                } else {
+                    add_random_tile(&mut moved);
+                    play(moved, new_score);
                 }
             }
+            Err(e) => {
+                println!("{e}");
+                thread::sleep(Duration::from_secs(1));
+                play(board, score)
+            }
         }
+    } else {
+        println!("Game Over!");
+        println!("Final Score: {score}");
     }
-
-    Ok(())
 }
 
 fn main() {
-    let mut field = vec![0; 4 * 4];
-    let mut score = 0;
-    let verbose = false;
+    let board = setup();
+    let score = 0usize;
 
-    'gameloop: loop {
-        match gen_rand_cell(&mut field) {
-            Ok(()) => {}
-            Err(msg) => panic!("{}", msg),
-        }
-
-        for move_type in [Move::Up, Move::Left, Move::Down, Move::Right] {
-            let mut test = field.clone();
-
-            match do_game_step(&move_type, &mut test, &mut score) {
-                Ok(()) => {}
-                Err(msg) => panic!("{}", msg),
-            }
-
-            if test != field {
-                break;
-            }
-
-            match move_type {
-                Move::Right => {
-                    match print_game(&mut field, &score, verbose, true) {
-                        Ok(()) => {}
-                        Err(msg) => panic!("{}", msg),
-                    }
-
-                    break 'gameloop;
-                }
-                _ => {}
-            }
-        }
-
-        match print_game(&mut field, &score, verbose, false) {
-            Ok(()) => {}
-            Err(msg) => panic!("{}", msg),
-        }
-
-        let test = field.clone();
-
-        while test == field {
-            match get_user_move() {
-                Ok(umove) => match umove {
-                    Move::Quit => {
-                        println!("\nGame Quit");
-                        println!("Final Score: {}", score);
-
-                        match print_game(&mut field, &score, verbose, true) {
-                            Ok(()) => {}
-                            Err(msg) => panic!("{}", msg),
-                        }
-
-                        break 'gameloop;
-                    }
-                    _ => {
-                        match print_game(&mut field, &score, verbose, false) {
-                            Ok(()) => {}
-                            Err(msg) => panic!("{}", msg),
-                        }
-
-                        match do_game_step(&umove, &mut field, &mut score) {
-                            Ok(()) => {}
-                            Err(msg) => panic!("{}", msg),
-                        }
-                    }
-                },
-                Err(msg) => panic!("{}", msg),
-            }
-        }
-    }
+    play(board, score);
 }
